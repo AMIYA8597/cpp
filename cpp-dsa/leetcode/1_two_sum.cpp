35537d5882674acdd96b4e1a3180f436bb2df04f
AMIYA8597 <chowdhuryamiya41@gmail.com>
2026-03-17 00:17:53 +0530

    1 two sum

---
+++ cpp-dsa/leetcode/1_two_sum.cpp
@@ -0,0 +1,235 @@
+// 1. Two Sum
+
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i =0; i<n; i++) {
+            for(int j =i+1; j<n; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map <int,int> mp;
+         for(int i=0;i<nums.size();i++)
+         {
+           int a=target-nums[i];
+           if(mp.find(a)!=mp.end())
+           {return {mp[a],i};}
+           mp[nums[i]]=i;
+         }
+         return{-1,-1};
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+
+        int i =0;
+        while(i < nums.size())
+        {
+            int remain = target - nums[i];
+            if(mp.find(remain) != mp.end())
+            {
+                return {mp[remain],i};
+            }
+            mp[nums[i]] = i;
+            i++;
+        }
+        return {0,0};
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen; // value -> index
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if (seen.find(complement) != seen.end()) {
+                return {seen[complement], i};
+            }
+            
+            seen[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            auto it = m.find(target - nums[i]);
+            if(it != m.end()){
+                return {it->second,i};
+            }
+            m[nums[i]] = i;
+        }
+        return {-1,-1};
+    }
+};
